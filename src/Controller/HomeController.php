0eb687c1aa420c91beb184dfab7fabbdf20d6444
inoriex <inoriex@gmail.com>
2022-04-22 09:22:18 +0200

    panier

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,11 +13,13 @@ class HomeController extends AbstractController
      */
     public function home(): Response
     {
-    {
+    
         $lorem= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea provident, sapiente magnam assumenda modi, alias doloribus error soluta consequatur ullam perferendis iste molestias fuga mollitia. Facere id corporis pariatur minus.";
         
-        return $this->render('home/home.html.twig', ['loremjinx' => $lorem]);
+        return $this->render('home/home.html.twig', [
+            'loremjinx' => $lorem
+        ]);
 
-    }
+    
     }
 }
